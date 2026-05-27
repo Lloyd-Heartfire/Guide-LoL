5142c3f947fc5abca46b675639b95ffa02312b26
Lloyd-Heartfire <beaudonloic@hotmail.com>
2026-05-27 14:43:24 +0200

    First bug patch

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { GameDataProvider } from "./context/GameDataContext";
 import Header from "./components/Header";
 import Home from "./pages/Home.tsx";
-// import Build from "./pages/Build";
+import Build from "./pages/Build";
 import './App.css';
+
+const basename = import.meta.env.BASE_URL.replace(/\/$/, "");
 
 export default function App() {
   return (
     <GameDataProvider>
-      <BrowserRouter basename="/lol-build-simulator">
+      <BrowserRouter basename={basename}>
         <div className="app">
           <div className="app_bg" />
           <Header />
@@ -16,7 +18,7 @@ export default function App() {
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/build" element={<Home />} />
-              {/* <Route path="/build/:champId" element={<Build />} /> */}
+              <Route path="/build/:champId" element={<Build />} />
               {/* <Route path="/synergies/:champId?" element={<Synergies />} /> */}
               {/* <Route path="/damage/:champId?" element={<Damage />} /> */}
               {/* <Route path="/duel" element={<Duel />} /> */}
@@ -31,5 +33,3 @@ export default function App() {
     </GameDataProvider>
   );
 }
-
-export default App
